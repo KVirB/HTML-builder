a7d1691162c93d491626f9d9158d4133d5111334
KVirB <dlaydrygaakk@mail.ru>
2024-01-17 11:19:07 +0300

    feat: 01-read-file complete

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path")
+
+const dirname = path.join(__dirname, "text.txt")
+const readStream = fs.createReadStream(dirname, 'utf8');
+
+readStream.on("data", (data) => {
+  console.log(data);
+});
