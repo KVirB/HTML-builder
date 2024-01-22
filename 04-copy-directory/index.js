e2c512891870711fcea25dbf0d62f3d7d5d16750
KVirB <dlaydrygaakk@mail.ru>
2024-01-22 23:21:03 +0300

    feat: 04-copy-directory complete

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirnameCopy = path.join(__dirname, 'files-copy');
+const dirname = path.join(__dirname, 'files');
+
+fs.stat(dirnameCopy, (err) => {
+  if (err) {
+    fs.mkdir(dirnameCopy, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log('Created directory files-copy');
+    });
+  }
+});
+
+fs.readdir(dirname, (err, files) => {
+  if (err) {
+    console.error(err);
+  }
+  files.forEach((file) => {
+    fs.copyFile(
+      `${__dirname}/files/${file}`,
+      `${__dirname}/files-copy/${file}`,
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      },
+    );
+  });
+  console.log('Files copied');
+});
