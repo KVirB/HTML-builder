9b4b63e84f3bb1a3d45250ff25d61c26f1c6a819
KVirB <dlaydrygaakk@mail.ru>
2024-01-23 23:59:52 +0300

    feat: 06-build-page generate index.html and style.css

---
+++ 06-build-page/index.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirnameDist = path.join(__dirname, 'project-dist');
+const dirnameStyle = path.join(dirnameDist, 'style.css');
+const dirnameStyles = path.join(__dirname, 'styles');
+const dirnameComponents = path.join(__dirname, 'components');
+
+fs.stat(dirnameDist, (err) => {
+  if (err) {
+    fs.mkdir(dirnameDist, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log('Created directory files-copy');
+    });
+  }
+
+  const styleFiles = fs.createWriteStream(dirnameStyle);
+  fs.readdir(dirnameStyles, (err, styles) => {
+    if (err) {
+      console.error(err);
+    }
+    styles.forEach((style) => {
+      if (style.split('.')[1] === 'css') {
+        const dirnameStyle = path.join(__dirname, `styles/${style}`);
+        const readStream = fs.createReadStream(dirnameStyle);
+
+        readStream.on('data', (data) => {
+          styleFiles.write(data + '\n');
+        });
+      }
+    });
+    console.log('style.css generate');
+  });
+
+  fs.readFile('06-build-page/template.html', 'utf-8', (err, template) => {
+    if (err) {
+      console.error(err);
+    }
+    let modifiedHTML = template;
+    fs.readdir(
+      dirnameComponents,
+      { withFileTypes: true },
+      (err, components) => {
+        if (err) {
+          console.error(err);
+        }
+        let componentsProcessed = 0;
+        components.forEach((component) => {
+          if (component.name.split('.')[1] === 'html') {
+            const dirnameIndex = path.join(
+              __dirname,
+              `components/${component.name}`,
+            );
+            const readStream = fs.createReadStream(dirnameIndex);
+
+            readStream.on('data', (data) => {
+              modifiedHTML = modifiedHTML.replace(
+                `{{${component.name.split('.')[0]}}}`,
+                data,
+              );
+            });
+
+            readStream.on('end', () => {
+              componentsProcessed++;
+              if (componentsProcessed === components.length) {
+                fs.writeFile(
+                  path.join(dirnameDist, 'index.html'),
+                  modifiedHTML,
+                  'utf-8',
+                  (err) => {
+                    if (err) {
+                      console.error(err);
+                    } else {
+                      console.log('index.html generated');
+                    }
+                  },
+                );
+              }
+            });
+          }
+        });
+      },
+    );
+  });
+});
